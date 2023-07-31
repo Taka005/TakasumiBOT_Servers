689307530da0d3b25d34e5458167cc41f73fac28
Taka005 <01taka005@gmail.com>
2023-08-01 00:17:57 +0900

    変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,8 @@ async function main(){
                     return `<div class="col-sm-6 Server">
                         <div class="card text-center">
                             <div class="card-body">
-                                <h5 class="card-title" id=${server.id}>${guild.name} - ${guild.memberCount}人${check}</h5>
+                                <h5 class="card-title" id=${server.id}>${guild.name} ${check}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">- ${guild.memberCount}人 -</h6>
                                 <p class="card-text">${server.text}</p>
                                 <a href="https://discord.gg/${server.code}" class="btn btn-secondary" target="_blank">サーバーに参加</a>
                                 <p class="card-text"><small class="text-muted">${time(new Date() - new Date(server.time))}前</small></p>
