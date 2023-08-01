58fa6cffe961bab23617a9a30a9a59633eeb6af3
Taka005 <takanosumika.sub@gmail.com>
2023-08-01 10:33:25 +0900

    完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,76 @@ async function main(){
             console.error(error);
             alert(`サーバーを取得出来ませんでした\n\nエラーコード: ${error}`);
         });
+    data.data.reverse();
 
-    await fetch("https://api.taka.cf/v1/discord/guilds")
+    const guilds = await fetch("https://api.taka.cf/v1/discord/guilds")
         .then(res=>res.json())
-        .then(res=>{
-            console.log(`${data.data.length}個のサーバーを取得しました`);
-            console.log(`${Object.keys(res.data).length}個のサーバー情報を取得しました`);
+        .catch(error=>{
+            console.error(error);
+            alert(`サーバー情報を取得出来ませんでした\n\nエラーコード: ${error}`);
+        });
+
+    console.log(`${data.data.length}個のサーバーを取得しました`);
+    console.log(`${Object.keys(guilds.data).length}個のサーバー情報を取得しました`);
+
+    document.querySelector(".serverList").insertAdjacentHTML("afterbegin",
+        data.data.map(server=>{
+            const guild = guilds.data[server.id];
+            const check = server.id === "987698915820335124"?`<span class="badge rounded-pill bg-primary">公式</span>`:"";
+
+            return `<div class="col-sm-6 Server">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title" id=${server.id}>${guild.name} ${check}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">- ${guild.memberCount}人 -</h6>
+                        <p class="card-text">${server.text}</p>
+                        <a href="https://discord.gg/${server.code}" class="btn btn-outline-secondary" target="_blank">サーバーに参加する</a>
+                        <p class="card-text"><small class="text-muted">${time(new Date() - new Date(server.time))}前</small></p>
+                    </div>
+                </div>
+            </div> `;
+        }).join("")
+    );
+
+    const serverForm = document.getElementById("serverForm");
+    const serverInput = document.getElementById("serverInput");
+
+    serverForm.addEventListener("submit",async(event)=>{
+        event.preventDefault();
+        document.querySelector(".serverList").innerHTML = "";
+        document.querySelector("#result").remove();
         
+        if(serverInput.value.length > 0){
+            const filter = Object.entries(guilds.data).filter(guild=>guild[1].name.indexOf(serverInput.value) != -1);
+
+            serverForm.insertAdjacentHTML("beforeend",`<div id="result" class="form-text">${data.data.filter(s=>filter.find(g=>g[0] === s.id)).length}件ヒットしました</div>`)
             document.querySelector(".serverList").insertAdjacentHTML("afterbegin",
-                data.data.reverse().map(server=>{
-                    const guild = res.data[server.id];
+                data.data.map(server=>{
+                    if(filter.find(g=>g[0] === server.id)){
+                        const guild = filter.find(g=>g[0] === server.id)[1];
+                        const check = server.id === "987698915820335124"?`<span class="badge rounded-pill bg-primary">公式</span>`:"";
+            
+                        return `<div class="col-sm-6 Server">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <h5 class="card-title" id=${server.id}>${guild.name} ${check}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">- ${guild.memberCount}人 -</h6>
+                                    <p class="card-text">${server.text}</p>
+                                    <a href="https://discord.gg/${server.code}" class="btn btn-outline-secondary" target="_blank">サーバーに参加する</a>
+                                    <p class="card-text"><small class="text-muted">${time(new Date() - new Date(server.time))}前</small></p>
+                                </div>
+                            </div>
+                        </div> `;
+                    }
+                }).join("")
+            );
+        }else{
+            serverForm.insertAdjacentHTML("beforeend",`<div id="result" class="form-text"></div>`)
+            document.querySelector(".serverList").insertAdjacentHTML("afterbegin",
+                data.data.map(server=>{
+                    const guild = guilds.data[server.id];
                     const check = server.id === "987698915820335124"?`<span class="badge rounded-pill bg-primary">公式</span>`:"";
-        
+
                     return `<div class="col-sm-6 Server">
                         <div class="card text-center">
                             <div class="card-body">
@@ -30,14 +88,12 @@ async function main(){
                     </div> `;
                 }).join("")
             );
-        })
-        .catch(error=>{
-            console.error(error);
-            alert(`サーバー情報を取得出来ませんでした\n\nエラーコード: ${error}`);
-        });
-
-    
+        }
+        serverInput.value = "";
+    });
 }
+
+main();
 
 function time(ms){  
     const t = Math.round(ms / 1000);
@@ -55,5 +111,3 @@ function time(ms){
         return `${h}時間`;
     }
 }
-
-main();
