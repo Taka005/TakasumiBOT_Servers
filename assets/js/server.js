05a6f1e7bb420beac20094ad10a47f63519510f7
Taka005 <01taka005@gmail.com>
2023-09-01 23:56:29 +0900

    fix

--- assets/js/server.js
+++ assets/js/server.js
@@ -8,9 +8,9 @@
             console.log(error);
         });
 
-    console.log(`${data.data.length}個のサーバーを取得しました`);
+    console.log(`${servers.data.length}個のサーバーを取得しました`);
 
-    const server = servers.find(server=>server.id === params.get("id"));
+    const server = servers.data.find(server=>server.id === params.get("id"));
     if(!server) return window.location = "./";
 
     const guild = await fetch(`https://api.taka.cf/v1/discord/guild?id=${server.id}`)
