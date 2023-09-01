946e6937e64ce59436f696327ed7b562f62cacdf
Taka005 <01taka005@gmail.com>
2023-09-01 23:32:43 +0900

    fix

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 
-if(params.has("id")) return location.href = "./";
+if(params.has("id")) return window.location = "./";
 
 const servers = await fetch("https://api.taka.cf/v1/server")
     .then(res=>res.json())
@@ -9,7 +9,7 @@ const servers = await fetch("https://api.taka.cf/v1/server")
     });
 
 const server = servers.find(server=>server.id === params.get("id"));
-if(!server[0]) return location.href = "./";
+if(!server[0]) return window.location = "./";
 
 const guild = await fetch(`https://api.taka.cf/v1/discord/guild?id=${server.id}`)
     .then(res=>res.json())
