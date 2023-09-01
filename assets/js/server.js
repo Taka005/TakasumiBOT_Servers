e644a2a46bfb7c3000e9803e3fb43aa9c06c54f3
Taka005 <01taka005@gmail.com>
2023-09-01 23:39:24 +0900

    fix

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,24 +1,26 @@
-const params = new URLSearchParams(window.location.search);
+(async function(){
+    const params = new URLSearchParams(window.location.search);
+    if(params.has("id")) return window.location = "./";
 
-if(params.has("id")) return window.location = "./";
-
-const servers = await fetch("https://api.taka.cf/v1/server")
-    .then(res=>res.json())
-    .catch(error=>{
-        console.log(error);
-    });
+    const servers = await fetch("https://api.taka.cf/v1/server")
+        .then(res=>res.json())
+        .catch(error=>{
+            console.log(error);
+        });
 
     console.log(`${data.data.length}個のサーバーを取得しました`);
 
-const server = servers.find(server=>server.id === params.get("id"));
-if(!server[0]) return window.location = "./";
+    const server = servers.find(server=>server.id === params.get("id"));
+    if(!server[0]) return window.location = "./";
 
-const guild = await fetch(`https://api.taka.cf/v1/discord/guild?id=${server.id}`)
-    .then(res=>res.json())
-    .catch(error=>{
-        console.log(error);
-    });
+    const guild = await fetch(`https://api.taka.cf/v1/discord/guild?id=${server.id}`)
+        .then(res=>res.json())
+        .catch(error=>{
+            console.log(error);
+        });
 
     console.log(`${guild.data.name}(${guild.data.id})を取得しました`);
 
-document.querySelector("title").innerText = guild.data.name;
+    document.querySelector("title").innerText = guild.data.name;
+
+})();
