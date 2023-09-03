08d550b7aa3437f6fcb722e8460936e71fda2d3d
Taka005 <takanosumika.sub@gmail.com>
2023-09-03 12:31:10 +0900

    完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ function ServerList(data){
                             <h5 class="card-title">${escape(server.name)} ${check}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">- ${server.count}人 -</h6>
                             <p class="card-text">${tag(escape(server.text))}</p>
-                            <a href="./server.html?id=${server.id}"></a>
+                            <a class="Link" href="./server.html?id=${server.id}"></a>
                         </div>
                         <a href="https://discord.gg/${server.code}" class="btn btn-outline-secondary" target="_blank">サーバーに参加する</a>
                         <p class="card-text"><small class="text-muted">${time(new Date() - new Date(server.time))}前</small></p>
@@ -85,9 +85,7 @@ function ServerList(data){
 }
 
 function tag(str){
-    return str.replace(/(#[^\s#]+)/g,(match,p1)=>{
-        return `<a class='tag' href='./?tag=${p1.replace("#","")}'>${p1}</a>`;
-    });
+    return str.replace(/(#[^\s#]+)/g,`<a class='tag' aria-label='タグ'>$&</a>`);
 }
 
 function time(ms){
